29d7243da32ae809533b8bb8bb81c53942b2c007
Serikoff <serikoff@li.ru>
2019-06-02 22:51:10 +0300

    minor code improvements

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,10 +4,12 @@ const expected = '{\n    host: hexlet.io\n  - timeout: 50\n  + timeout: 20\n  - 
 
 const expected2 = '{\n    common: {\n        setting1: Value 1\n      - setting2: 200\n      - setting3: true\n      + setting3: {\n            key: value\n        }\n        setting6: {\n            key: value\n          + ops: vops\n        }\n      + follow: false\n      + setting4: blah blah\n      + setting5: {\n            key5: value5\n        }\n    }\n    group1: {\n      - baz: bas\n      + baz: bars\n        foo: bar\n      - nest: {\n            key: value\n        }\n      + nest: str\n    }\n  - group2: {\n        abc: 12345\n    }\n  + group3: {\n        fee: 100500\n    }\n}';
 
+const location = '__tests__/__fixtures__/';
+
 test.each([
-  ['__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json'],
-  ['__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml'],
-  ['__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini'],
+  [`${location}before.json`, `${location}after.json`],
+  [`${location}before.yml`, `${location}after.yml`],
+  [`${location}before.ini`, `${location}after.ini`],
 ])(
   '.add(%s, %s)',
   (a, b) => {
@@ -16,9 +18,9 @@ test.each([
 );
 
 test.each([
-  ['__tests__/__fixtures__/beforeCurrying.json', '__tests__/__fixtures__/afterCurrying.json'],
-  ['__tests__/__fixtures__/beforeCurrying.yml', '__tests__/__fixtures__/afterCurrying.yml'],
-  ['__tests__/__fixtures__/beforeCurrying.ini', '__tests__/__fixtures__/afterCurrying.ini'],
+  [`${location}beforeCurrying.json`, `${location}afterCurrying.json`],
+  [`${location}beforeCurrying.yml`, `${location}afterCurrying.yml`],
+  [`${location}beforeCurrying.ini`, `${location}afterCurrying.ini`],
 ])(
   '.add(%s, %s)',
   (a, b) => {
